53f85f951e56acdab0fea8a0005f1a960e01ee97
Ubuntu <ubuntu@ip-172-31-1-115.ec2.internal>
2021-10-24 17:47:18 +0000

    temp

--- Benchmark/XLnet-huggingface/model-inference.py
+++ Benchmark/XLnet-huggingface/model-inference.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, pipeline
-import torch
+## import torch
 
 tokenizer = GPT2Tokenizer.from_pretrained("xlnet-base-cased")
 model = GPT2LMHeadModel.from_pretrained("xlnet-base-cased", pad_token_id=tokenizer.eos_token_id)
