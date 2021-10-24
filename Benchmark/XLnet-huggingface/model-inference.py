c9547a6dec06a5be6b8d6814ca68b36918e8a181
Jerry Pan <jerrypan2718@berkeley.edu>
2021-10-24 10:24:07 -0700

    1/10 setup for XLNet

--- Benchmark/XLnet-huggingface/model-inference.py
+++ Benchmark/XLnet-huggingface/model-inference.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, pipeline
+import torch
+
 tokenizer = GPT2Tokenizer.from_pretrained("xlnet-base-cased")
 model = GPT2LMHeadModel.from_pretrained("xlnet-base-cased", pad_token_id=tokenizer.eos_token_id)
+
+
